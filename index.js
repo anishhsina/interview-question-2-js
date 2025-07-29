437ca7360d390f3e710414409007763ee696b056
anishhsina <135202184+anishhsina@users.noreply.github.com>
2025-07-29 23:01:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+// let result = 15;
+// let result = parseInt(prompt("Enter the result (0-50):"));
+// let grade = "";
+
+// switch (true) {
+//   case result <= 10:
+//     grade = "F";
+//     break;
+//   case result <= 20:
+//     grade = "D";
+//     break;
+//   case result <= 30:
+//     grade = "C";
+//     break;
+//   case result <= 40:
+//     grade = "B";
+//     break;
+//   case result <= 50:
+//     grade = "A";
+//     break;
+//   default:
+//     console.log("Invalid result");
+//     break;
+// }
+
+// console.log(`Grade: ${grade}`);
+
+// let num = 5;
+// let month = "";
+
+// switch (num) {
+//   case 1:
+//     month = "January";
+//     break;
+
+//   case 2:
+//     month = "February";
+//     break;
+
+//   case 3:
+//     month = "March";
+//     break;
+
+//   case 4:
+//     month = "April";
+//     break;
+
+//   case 5:
+//     month = "May";
+//     break;
+
+//   case 6:
+//     month = "June";
+//     break;
+
+//   case 7:
+//     month = "July";
+//     break;
+
+//   case 8:
+//     month = "August";
+//     break;
+
+//   case 9:
+//     month = "September";
+//     break;
+
+//   case 10:
+//     month = "October";
+//     break;
+
+//   case 11:
+//     month = "November";
+//     break;
+
+//   case 12:
+//     month = "December";
+//     break;
+// }
+
+// console.log(month);
+
+// let obj = {
+//   name: "Anish",
+//   age: 25,
+//   city: "Kathmandu",
+//   email: "anish@gmail.com",
+// };
+
+// for (let key in obj) {
+//   console.log(`${key}: ${obj[key]}`);
+// }
+
+let num = parseInt(prompt("Enter a number:  "));
+for (let i = 1; i <= 10; i++) {
+  console.log(`${num} x ${i} = ${num * i}`);
+}
